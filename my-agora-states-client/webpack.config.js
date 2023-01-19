a094b5a62a0cf989da70feb51f91724bf55b7649
wangamy0222 <wangamyy222@gmail.com>
2023-01-19 14:33:42 +0900

    클라이언트, docs 수정

--- my-agora-states-client/webpack.config.js
+++ my-agora-states-client/webpack.config.js
@@ -1,52 +1,38 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// Node.js - os 모듈 불러오기
-const os = require("os");
-// CssMinimizerPlugin 모듈 불러오기
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "/docs"),
-    filename: "[name].bundle.js",
-    clean: true,
+    filename: "main.js",
+    path: path.resolve(__dirname, "docs"),
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        use: ["babel-loader"],
-        exclude: /node_modules/,
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+          },
+        },
       },
       {
         test: /\.css$/,
+        use: ["style-loader", "css-loader"],
         exclude: /node_modules/,
-        use: [
-          isDevMode ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
-        ],
       },
-    ],
-  },
-  optimization: {
-    // 압축
-    minimize: isDevMode ? false : true,
-    // 미니마이저
-    minimizer: [
-      // 플러그인 인스턴스 생성
-      new CssMinimizerPlugin({
-        // CPU 멀티 프로세서 병렬화 옵션 (기본 값: true)
-        parallel: os.cpus().length - 1,
-      }),
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
+      template: path.resolve(__dirname, "public", "index.html"),
     }),
   ],
+  mode: "production",
 };
